ec8725435802621002e98b946aa834a0321b0fc5
Daniel <daniel.jank@protonmail.com>
2023-03-30 17:54:04 +0200

    config: Now fully functional

--- lib/config.py
+++ lib/config.py
@@ -35,55 +35,72 @@ CONFIG_DEFAULT = {
 		"debug": {
 			"value": False,
 			"short": "v",
-			"action": "count",
+			"no_value": True,
 			"desc": "more debugging, show core exceptions"
 		},
 	}
 }
 
+
 class Config:
 	def __init__(self):
-		parser = argparse.ArgumentParser(description="Status - simple & convenient way to monitor your machine.")
+		parser = argparse.ArgumentParser(description="Status - simple and lightweight system monitoring tool for small homeservers.")
+		conf_map = {}
 
 		# Read config from arguments
 		for section in CONFIG_DEFAULT:
 			for key in CONFIG_DEFAULT[section]:
 				a = CONFIG_DEFAULT[section][key]
-				value = a["value"]
-				desc = a["desc"]
-				short = f"-{a['short']}"
-				action = a["action"] if "action" in a else None
+				no_value = a["no_value"] if "no_value" in a else False
+				action = "count" if no_value else None
+				k = key.replace("_", "-")
 
-				key = key.replace("_", "-")
-				if a['short']: parser.add_argument(short, f"--{section}-{key}", action=action, help=desc)
-				else: parser.add_argument(f"--{section}-{key}", action=action, help=desc)
+				if a['short']: arg = parser.add_argument(f"-{a['short']}", f"--{section}-{k}", action=action)
+				else: arg = parser.add_argument(f"--{section}-{k}", action=action)
+				arg.help = a["desc"]
+				if not action: arg.metavar = key.upper()
+
+				conf_map[f"{section}_{key}"] = f"{section}.{key}"
 
 		parser.add_argument("-c", "--config", help="custom path to config file")
-		parser.add_argument("--no-config", action="count", help="ignore config files: don't read/save them")
+		parser.add_argument("--no-config", action="count", help="ignore config files; don't read nor save them")
 		self.config = vars(parser.parse_args())
 
+
 		# Read config from environment variables
-		ENV = {}
+		ENV = "STATUS"
 		for key in environ:
-			if key.startswith("STATUS_"):
-				var_name = key.replace("STATUS_", "").lower()
+			if key.startswith(f"{ENV}_"):
+				var_name = key.replace(f"{ENV}_", "").lower()
 				if var_name in self.config and self.config[var_name] == None:
 					self.config[var_name] = environ[key]
+
 
 		# Read config from file
 		path = self.config["config"]
 		path = path if path else "config.json"
 		if not self.config["no_config"] and path:
 			try:
-				f = open(path, "r").read()
-				print("Config files are not implemented yet.")
+				config_file = json.loads(open(path, "r").read())
+				for section in config_file:
+					for key in config_file[section]:
+						CONFIG_DEFAULT[section][key]["value"] = config_file[section][key]
 
 			except FileNotFoundError:
+				config_file = {}
+				for entry in conf_map:
+					e = conf_map[entry].split(".")
+					section = e[0]
+					key = e[1]
+					if section not in config_file:
+						config_file[section] = {}
+					config_file[section][key] = CONFIG_DEFAULT[section][key]["value"]
+
 				with open(path, "w") as file:
-					print("Config file not found.")
-					print(f"Saving current configuration to {path}...")
-					file.write(json.dumps(self.config, indent=4))
+					print(f"Config file not found. Saving default configuration to {path}...")
+					file.write(json.dumps(config_file, indent=4))
 					file.close()
+
 
 		# Fallback values from default config
 		for section in CONFIG_DEFAULT:
@@ -92,6 +109,10 @@ class Config:
 				var_name = f"{section}_{key}"
 				if self.config[var_name] == None:
 					self.config[var_name] = value
+
+
+		if self.config["misc_debug"]:
+			print("Debug is enabled.")
 
 
 	def get(self, section, key):
